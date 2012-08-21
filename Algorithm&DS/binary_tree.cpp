dcad150f22b613700ea14b405bc41e12f12e1892
uestczhangchao <uestczhangchao@gmail.com>
2012-08-21 17:09:51 +0800

    btree

---
+++ Algorithm&DS/binary_tree.cpp
@@ -0,0 +1,95 @@
+﻿/*
+author:justinzhang
+email:uestczhangchao@gmail.com
+time:2012-8-20 15:52:27
+des.:using non recursive method to traverse binary tree
+*/
+#include <iostream>
+using namespace std;
+
+template<typename T> class btree
+{
+public:
+	T data;
+	btree *lchild, *rchild;
+};
+
+/*	create binary tree, with one parameter.
+	the binary tree is inputed through pre order,
+	1
+   2 3, is input as 1 2 -1 -1 3 -1 -1,(-1 represent nil node);
+*/
+btree<double> * create_btr(btree<double> **root)
+{
+	double data ;
+	cin>>data;
+	/* -1.0 represent the nil node */
+	if(data == -1.0) 
+	{
+		*root=NULL;
+	}
+	else
+	{
+		*root = new btree<double>();
+		(*root)->data = data;
+		(*root)->lchild = create_btr(&(*root)->lchild);
+		(*root)->rchild = create_btr(&(*root)->rchild);		
+	}
+	return *root;
+}
+
+/*create binary tree without parameter!*/
+btree<double>* create_btr()
+{
+	double data;
+	cin >> data;
+	if(data == -1)
+		return NULL; //return nill node
+	else
+	{
+		btree<double> *T = new btree<double>;
+		T->data = data;
+		T->lchild = create_btr();
+		T->rchild = create_btr();
+		return T;
+	}
+}
+
+void preorder(btree<double> *r)
+{
+	if(r==NULL)
+		return;
+	cout << r->data << endl;
+	preorder(r->lchild);
+	preorder(r->rchild);
+}
+
+void inorder(btree<double> *r)
+{
+	if(NULL==r)
+		return;
+	inorder(r->lchild);
+	cout << r->data<<endl;
+	inorder(r->rchild);
+}
+void postorder(btree<double> *r)
+{
+	if(NULL==r)
+		return ;
+	postorder(r->lchild);
+	postorder(r->rchild);
+	cout << r->data<<endl;
+}
+
+
+int main()
+{
+	btree<double> *root = NULL;
+	create_btr(&root);
+	//btree<double> *T = create_btr();
+	preorder(root);
+	inorder(root);
+	postorder(root);
+	//inorder(T);
+	return 0;
+}
