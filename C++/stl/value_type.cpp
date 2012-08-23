5dd928f088a31740abd4e1ebb462611f9bbffe9c
uestczhangchao <uestczhangchao@gmail.com>
2012-08-23 11:47:57 +0800

    iterator_traits

--- C++/stl/value_type.cpp
+++ C++/stl/value_type.cpp
@@ -21,17 +21,19 @@ struct iterator_traits
 {
 	typedef typename I::value_type value_type;	
 };
+
+template <class T>
+struct iterator_traits<T*>
+{
+	typedef T value_type;
+};
+
 template <class I>
 typename iterator_traits<I>::value_type
 func(I ite)
 {
 	return *ite;
 }
-template <class T>
-struct iterator_traits<T*>
-{
-	typedef T value_type;
-};
 
 int main()
 {
