e34d567247b81423f815998136409693a7aa100a
uestczhangchao <uestczhangchao@gmail.com>
2012-08-30 19:34:50 +0800

    ...

--- Algorithm&DS/binary_search.cpp
+++ Algorithm&DS/binary_search.cpp
@@ -8,16 +8,39 @@ desc: binary search from the aocp
 #include <iostream>
 #include <ctime>
 #include <cmath>
+#include <cstdlib>
 using namespace std;
 
 template <class type>
 type random(type start, type end)
 {
-	srand((unsigned)time(0)*rand());
+	srand((int)time(0)*rand());
 	return (start + (end-start)*rand()/(RAND_MAX+1.0));
 }
 
 int main()
 {
+	
+	auto_ptr<double> data(new double[20]);
+	int i = 0;
+	for(int i=0; i<20; i++)
+	{
+		data[i] = random<double>(1.0,20.0);	
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
