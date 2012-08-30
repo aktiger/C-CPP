869c7496c12307f8418f1b8286c2fb75f7ddb1a7
uestczhangchao <uestczhangchao@gmail.com>
2012-08-30 22:21:15 +0800

    fork problem

---
+++ Linux programming/fork.c
@@ -0,0 +1,24 @@
+﻿/*
+Author:Justinzhang
+email:uestczhangchao@gmail.com
+time:2012-8-30 22:17:23
+desc：fork problem, from www.qshpan.com
+*/
+
+#include <stdio.h>
+int sum;
+int main()
+{
+	int i = 0;
+	sum = 0;
+	fork();
+	for(i=0; i<5; i++)
+	{
+		printf("%d\n",i);
+		fflush();
+		sum += i;
+	}
+	printf("sum = %d\n", sum);
+	
+	exit(0);
+}
