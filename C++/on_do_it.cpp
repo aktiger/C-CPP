cd296b1ab5fa03e97ddda2d9d062dc4c90e65b05
uestczhangchao <uestczhangchao@gmail.com>
2012-09-03 22:36:30 +0800

    on do it

---
+++ C++/on_do_it.cpp
@@ -0,0 +1,50 @@
+﻿/*
+Author:	Justinzhang
+Email:	uestczhangchao@gmail.com
+Time:	2012-9-3 22:13:11
+Desc:	this program is from tencent test...
+		some what a fun program
+*/
+
+#include <iostream>
+using namespace std;
+template<typename T>
+class A
+{
+public:
+	void doint()
+	{
+		T *pT = (T *) this;
+		pT->ondoit();
+	}
+	void ondoit()
+	{
+		cout << " A:: ondoit" << endl;
+	}
+	
+};
+
+class B:public A<B>
+{
+	public:
+		void ondoit()
+		{
+			cout << "B<A>::ondoit" << endl;
+		}
+};
+
+int main()
+{
+	B b;
+	b.ondoit();
+	A<B> a;
+	a.doint();
+	
+	return 0;
+}
+
+
+
+
+
+
